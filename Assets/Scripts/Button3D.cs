7c46cecd46d46bdb6eddbdf2b4e6e11e5ccd26a9
Marcelo de Freitas Rigon <arendhil@gmail.com>
2018-01-28 14:12:14 -0200

    Menu OK.

---
+++ Assets/Scripts/Button3D.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button3D : MonoBehaviour {
+	public Camera cam;
+
+	void Update () {
+		if (Input.GetMouseButtonDown(0)) {
+			Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+			RaycastHit hit;
+			if (Physics.Raycast(ray, out hit)) {
+				//Debug.Log("Mouse Down Hit the following object: " + hit.collider.name);
+				if (hit.collider.gameObject == this.gameObject)
+					Click();
+			}
+		}
+	}
+
+	virtual public void Click() {
+
+	}
+}
